ee5af82354385bf396975298f19ff8957a209953
Mufacoderz <mf9555759@gmail.com>
2026-03-27 20:18:48 +0800

    sesuaikan ulang

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -1,24 +1,20 @@
 <nav class="navbar navbar-expand-lg w-100 position-fixed top-0 start-0 border-bottom" style="z-index: 1030;">
-    <div class="container py-0">
+    <div class="container py-0 d-flex align-items-center">
 
-        <a class="navbar-brand fw-bold fs-3 text-decoration-none text-white" href="#">
+        <a class="navbar-brand fw-bold fs-4 text-decoration-none text-white" href="#">
             Nusa Interiors
         </a>
 
-        <button class="navbar-toggler border-0 shadow-none"
+        <button class="navbar-toggler border-0 shadow-none ms-auto"
                 type="button"
                 data-bs-toggle="collapse"
-                data-bs-target="#mainNav"
-                aria-controls="mainNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                
-                <i class="fa-solid fa-bars text-white fs-4"></i>
+                data-bs-target="#mainNav">
+            <i class="fa-solid fa-bars text-white fs-4"></i>
         </button>
 
-        <div class="collapse navbar-collapse p-3 mt-2 mx-3 rounded" id="mainNav">
+        <div class="collapse navbar-collapse align-items-center justify-content-between mt-3 mt-lg-0" id="mainNav">
 
-            <ul class="navbar-nav mx-auto gap-1">
+            <ul class="navbar-nav mx-auto gap-1 text-center text-lg-start">
                 <li class="nav-item">
                     <a class="nav-link fw-semibold small px-3 text-white" href="#">Home</a>
                 </li>
@@ -33,8 +29,8 @@
                 </li>
             </ul>
 
-            <div class="mt-3 mt-lg-0">
-                <a href="#" class="btn text-white fw-bold small px-4 py-2 rounded-pill golden-btn">
+            <div class="consult-btn mt-3 mt-lg-0 text-center text-lg-end">
+                <a href="#" class="btn text-white fw-bold small px-4 py-2 rounded-pill golden-btn w-100 w-lg-auto">
                     Consult Us
                 </a>
             </div>
